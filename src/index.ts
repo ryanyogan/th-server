948468261176c86f4c45becf49295bfb36443941
Ryan Yogan <ryanyogan@gmail.com>
2020-12-02 12:19:41 -0600

    basic little rest ep's

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import express from "express";
+import { listings } from "./listings";
 
 const app = express();
 const port = 9000;
 
+app.use(express.json());
 app.get("/", (_req, res) => res.send("hello world"));
+app.get("/listings", (_req, res) => {
+  res.send(listings);
+});
+
+app.post("/delete-listing", (req, res) => {
+  const id: string = req.body.id;
+
+  listings.forEach((listing, idx) => {
+    if (listing.id === id) {
+      return res.send(listings.splice(idx, 1));
+    }
+  });
+
+  return res.send("failed to delete listing");
+});
 
 app.listen(port);
 
