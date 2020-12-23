c4f5277fcc82ba2c656aa4e3ec65b25d662a2361
Ryan Yogan <ryanyogan@gmail.com>
2020-12-23 08:14:27 -0600

    updated resolvers for the user profile page

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,5 +1,12 @@
 import merge from "lodash.merge";
+import { bookingResolvers } from "./Booking";
+import { listingResolvers } from "./Listing";
 import { userResolvers } from "./User";
 import { viewerResolvers } from "./Viewer";
 
-export const resolvers = merge(viewerResolvers, userResolvers);
+export const resolvers = merge(
+  viewerResolvers,
+  userResolvers,
+  listingResolvers,
+  bookingResolvers
+);
